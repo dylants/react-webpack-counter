aacdc411e4c5ad1ece9d49cf5e9e63627c203c19
Dylan Smith <dylantsmith@gmail.com>
2016-01-08 17:14:18 -0600

    Initial commit
    Create the initial files which include:
    * ESLint and JSCS configuration
    * Babel support for both server side and client side files
    * A basic webpack configuration with separate support for development
    and production environments. This has been pulled from many starter
    kits and online documentation.
    * An express application which for now only serves the html page.
    * Build and production script support.

---
+++ server.js
@@ -0,0 +1,49 @@
+/* eslint no-console:0 */
+
+import path from 'path';
+import express from 'express';
+import webpack from 'webpack';
+import webpackDevMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
+import webpackConfig from './webpack.config.js';
+
+// default to development environment
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const isDev = NODE_ENV !== 'production';
+// default to port 3000 if not supplied
+const port = process.env.PORT || 3000;
+const app = express();
+
+if (isDev) {
+  const compiler = webpack(webpackConfig);
+  const middleware = webpackDevMiddleware(compiler, {
+    publicPath: webpackConfig.output.publicPath,
+    contentBase: 'src',
+    stats: {
+      colors: true,
+      hash: false,
+      timings: true,
+      chunks: false,
+      chunkModules: false,
+      modules: false,
+    },
+  });
+
+  app.use(middleware);
+  app.use(webpackHotMiddleware(compiler));
+} else {
+  app.use(express.static(__dirname + '/dist'));
+}
+
+// for now, all requests render the index page
+app.all('*', function response(req, res) {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+app.listen(port, function onStart(err) {
+  if (err) {
+    console.log(err);
+  }
+
+  console.log(`Express server listening on port ${port} in ${NODE_ENV} environment`);
+});
---
+++ webpack.config.js
@@ -0,0 +1,151 @@
+/* eslint strict:0 */
+
+'use strict';
+
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+// default the environment to development
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const appPath = path.join(__dirname, 'app');
+const assetsPath = path.join(__dirname, 'dist');
+const publicPath = '/';
+
+function getPlugins() {
+  // These plugins are used in all environments
+  const plugins = [
+
+    // https://github.com/ampedandwired/html-webpack-plugin
+    new HtmlWebpackPlugin({
+      template: 'app/index.tpl.html',
+      inject: 'body',
+      filename: 'index.html',
+    }),
+
+    // http://webpack.github.io/docs/list-of-plugins.html#occurenceorderplugin
+    new webpack.optimize.OccurenceOrderPlugin(),
+
+    // http://webpack.github.io/docs/list-of-plugins.html#noerrorsplugin
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(NODE_ENV),
+    }),
+  ];
+
+  // add plugins that should be used only in certain environments
+  switch (NODE_ENV) {
+    case 'production':
+
+      // http://webpack.github.io/docs/list-of-plugins.html#dedupeplugin
+      plugins.push(new webpack.optimize.DedupePlugin());
+
+      // http://webpack.github.io/docs/list-of-plugins.html#uglifyjsplugin
+      plugins.push(new webpack.optimize.UglifyJsPlugin({
+        minimize: true,
+        sourceMap: true,
+      }));
+      break;
+    case 'development':
+
+      // http://webpack.github.io/docs/list-of-plugins.html#hotmodulereplacementplugin
+      plugins.push(new webpack.HotModuleReplacementPlugin());
+
+      // http://webpack.github.io/docs/list-of-plugins.html#noerrorsplugin
+      plugins.push(new webpack.NoErrorsPlugin());
+      break;
+    default:
+      throw new Error('Unknown environment: ' + NODE_ENV);
+  }
+
+  return plugins;
+}
+
+function getLoaders() {
+  const loaders = [
+    {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['es2015', 'stage-0', 'react'],
+      },
+    }, {
+      test: /(\.scss)$/,
+      exclude: /node_modules/,
+      loaders: ['style', 'css', 'sass'],
+    },
+  ];
+
+  return loaders;
+}
+
+function getEntry() {
+  const entry = [];
+
+  // hot reload only when in dev environment
+  if (NODE_ENV === 'development') {
+    // https://github.com/glenjamin/webpack-hot-middleware#config
+    entry.push('webpack-hot-middleware/client?reload=true');
+  }
+
+  // add our entry point for the client web app
+  entry.push(path.join(appPath, 'index.js'));
+
+  return entry;
+}
+
+function getOutput() {
+  let output;
+
+  // in production, we need a special output object
+  if (NODE_ENV === 'production') {
+    output = {
+      path: assetsPath,
+      filename: '[name]-[hash].min.js',
+    };
+  } else {
+    output = {
+      path: assetsPath,
+      publicPath,
+      filename: '[name].js',
+    };
+  }
+
+  return output;
+}
+
+module.exports = {
+  // necessary per https://webpack.github.io/docs/testing.html#compile-and-test
+  target: NODE_ENV === 'test' ? 'node' : 'web',
+
+  debug: true,
+
+  // more info: https://webpack.github.io/docs/build-performance.html#sourcemaps
+  // more info: https://webpack.github.io/docs/configuration.html#devtool
+  devtool: NODE_ENV === 'production' ? 'source-map' : 'eval-source-map',
+
+  // set to false to see a list of every file being bundled.
+  noInfo: true,
+
+  resolve: {
+    // defines where the code resides
+    root: appPath,
+
+    // lists file types that can have optional extensions
+    extensions: ['', '.js', '.jsx'],
+
+    // sets a base dir for imports
+    modulesDirectories: ['node_modules', 'app'],
+  },
+
+  plugins: getPlugins(),
+
+  module: {
+    loaders: getLoaders(),
+  },
+
+  entry: getEntry(),
+
+  output: getOutput(),
+
+};
